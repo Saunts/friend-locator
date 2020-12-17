ca0e51ce872a38f3ba3c4c4c9a650988eb6ee792
Saunts <Saunts@users.noreply.github.com>
2020-12-17 15:00:57 +0700

    Test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAGIeQmYaNXdV5doK4JcEYMjvj79NY6jE0",
+    authDomain: "friend-locator-9921d.firebaseapp.com",
+    projectId: "friend-locator-9921d",
+    storageBucket: "friend-locator-9921d.appspot.com",
+    messagingSenderId: "607223766577",
+    appId: "1:607223766577:web:dc3fa6e6af4dafb3543f10",
+    measurementId: "G-4C1PS62CTW"
+  }
 };
 
 /*
